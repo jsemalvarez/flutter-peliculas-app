7751fda3cd6766353ea333ddaf1543226d2f2428
jose.alvarez3 <jose.alvarez@conexa.ai>
2023-02-02 21:21:47 -0300

    primeros pasos

---
+++ lib/screens/home_screens.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Peliculas en cines"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
+        ],
+      ),
+      body: Column(
+        children: const [CardSwiper()],
+      ),
+    );
+  }
+}
